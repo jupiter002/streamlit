012509e48eedf34acbcc68f24fe23ec3d861e2c6
Moonsun23 <socoooool90@gmail.com>
2023-10-20 18:43:42 +0900

    중복단어테스트 및 제거

--- mdata/joongbok_vocab.py
+++ mdata/joongbok_vocab.py
@@ -1,20 +1,60 @@
+# txt 파일 중복삭제
+def remove_duplicates(sw):
+    with open(sw, 'r', encoding='utf-8') as f:  # encoding='utf-8'로 변경
+        lines = f.readlines()
+
+    # 중복확인
+    unique_lines = set()
+    cleaned_lines = []
+
+    for line in lines:
+        # 이미 unique_lines에 존재하지 않는 라인만 추가
+        if line not in unique_lines:
+            cleaned_lines.append(line)
+            unique_lines.add(line)
+
+    # 결과를 다시 파일에 쓰기
+    with open(sw, 'w', encoding='utf-8') as f:  # encoding='utf-8'로 변경
+        f.writelines(cleaned_lines)
+
+    print(f"{len(lines) - len(cleaned_lines)}개의 중복된 라인을 제거했습니다.")
+
+# 사용 예시
+sw = '../data/stopwords-kor.txt'
+remove_duplicates(sw)
+
+
+# 파일의 타입 확인하기
+# import chardet
+#
+# def detect_encoding(filename):
+#     with open(filename, 'rb') as f:
+#         result = chardet.detect(f.read())
+#     return result['encoding']
+#
+# filename = '../data/stopwords-kor.txt'
+# encoding = detect_encoding(filename)
+# print(f"Detected encoding: {encoding}")
+
+
+
 # 중복요소 삭제
-import json
-
-# JSON 파일 불러오기
-with open('../data/motel_list_last.json', 'r', encoding='utf-8') as f:
-    motel_data = json.load(f)
-
-# 223번 항목 삭제
-motel_data = [motel for motel in motel_data if motel['숙소번호'] != 223]
-
-# 다시 넘버링
-for idx, motel in enumerate(motel_data, start=1):
-    motel['숙소번호'] = idx
-
-# 수정된 데이터를 JSON 파일에 저장
-with open('../data/motel_list_last.json', 'w', encoding='utf-8') as f:
-    json.dump(motel_data, f, ensure_ascii=False, indent=4)
+# import json
+#
+# # JSON 파일 불러오기
+# with open('../data/motel_list_last.json', 'r', encoding='utf-8') as f:
+#     motel_data = json.load(f)
+#
+# # 223번 항목 삭제
+# motel_data = [motel for motel in motel_data if motel['숙소번호'] != 223]
+#
+# # 다시 넘버링
+# for idx, motel in enumerate(motel_data, start=1):
+#     motel['숙소번호'] = idx
+#
+# # 수정된 데이터를 JSON 파일에 저장
+# with open('../data/motel_list_last.json', 'w', encoding='utf-8') as f:
+#     json.dump(motel_data, f, ensure_ascii=False, indent=4)
 
 # 중복 모텔 체크
 # import json
